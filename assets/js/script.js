ad641e582b84a6a2cc7c4b873de8f1697bad3877
Hector Nava <hector.r.nava44@gmail.com>
2021-11-20 17:35:29 -0700

    Added current weather details to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,25 @@ $(document).ready(function() {
     const $searchForm = $('#searchForm');
     const $currNameEl = $('#currentName');
     const $currIconEl = $('#currentIcon');
+    const $currTempEl = $('#temp');
+    const $currWindEl = $('#wind');
+    const $currHumidEl = $('#humid');
+    const $currUviEl = $('#uvi');
 
-    function oneCallRequest(cityLat, cityLon, callback) {
-        let requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLat + '&lon=' + cityLon + '&exclude=minutely,hourly&appid=' + apiKey;
+    function oneCallRequest(cityLat, cityLon) {
+        let requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLat + '&lon=' + cityLon + '&units=imperial&exclude=minutely,hourly&appid=' + apiKey;
 
         return $.ajax({
             url: requestUrl,
             method: 'GET',
         }).then(function(response) {
             let details = {
-                date: moment.unix(response.current.dt).format('MM/DD/YYYY'),
-                iconUrl: 'http://openweathermap.org/img/w/' + response.current.weather[0].icon + '.png'
+                currDate: moment.unix(response.current.dt).format('MM/DD/YYYY'),
+                currTemp: response.current.temp,
+                currWind: response.current.wind_speed,
+                currHumid: response.current.humidity,
+                currUvi: response.current.uvi,
+                currIconUrl: 'http://openweathermap.org/img/w/' + response.current.weather[0].icon + '.png'
             };
             return details;
         });
@@ -40,16 +48,30 @@ $(document).ready(function() {
                     let cityName = response[0].name;
                     let cityCountry = response[0].country;
 
-                    $currIconEl.attr('src', results.iconUrl);
+                    $currIconEl.attr('src', results.currIconUrl);
 
                     if ('state' in response[0]) {
                         let cityState = response[0].state;
-                        $currNameEl.text(cityName + ', ' + cityState + ', ' + cityCountry + ' (' + results.date + ')')
+                        $currNameEl.text(cityName + ', ' + cityState + ', ' + cityCountry + ' (' + results.currDate + ')')
                             .append($currIconEl);
                     } else {
-                        $currNameEl.text(cityName + ', ' + cityCountry + ' (' + results.date + ')')
+                        $currNameEl.text(cityName + ', ' + cityCountry + ' (' + results.currDate + ')')
                             .append($currIconEl);
                     };
+
+                    // Populating current weather details for selected location
+                    $currTempEl.text(results.currTemp + '℉');
+                    $currWindEl.text(results.currWind + ' MPH');
+                    $currHumidEl.text(results.currHumid + '%');
+                    $currUviEl.text(results.currUvi)
+                    if (results.currUvi <= 2) {
+                        $currUviEl.addClass('bg-success');
+                    } else if (results.currUvi > 2 && results.currUvi <= 7) {
+                        $currUviEl.addClass('bg-warning');
+                    } else {
+                        $currUviEl.addClass('bg-danger');
+                    };
+
                 });
             } else {
                 return console.log('no');
