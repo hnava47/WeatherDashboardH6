15d5ea5ac81268bc915c7efdd2e14ae5c8ce8185
Hector Nava <hector.r.nava44@gmail.com>
2021-11-23 11:11:39 -0700

    Add favorite functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
     const $minusEl = $('#minus');
     const $favoritesEL = $('#favorites');
     const $currNameEl = $('#currentName');
+    const $currDateEl = $('#currentDate');
     const $currIconEl = $('#currentIcon');
     const $currTempEl = $('#temp');
     const $currWindEl = $('#wind');
@@ -33,7 +34,6 @@ $(document).ready(function() {
             };
         }
         let cityList = [...citySet];
-        console.log(cityList)
         $(function() {
             $searchInput.autocomplete({
                 minLength: 4,
@@ -100,61 +100,88 @@ $(document).ready(function() {
             method: 'GET'
         }).then(function(response) {
             $errBanner.hide();
-            if (response.length > 0) {
-                oneCallRequest(response[0].lat, response[0].lon).then(function(results) {
-                    let cityName = response[0].name;
-                    let cityCountry = response[0].country;
+            oneCallRequest(response[0].lat, response[0].lon).then(function(results) {
+                let cityName = response[0].name;
+                let cityCountry = response[0].country;
 
-                    $currIconEl.attr('src', results.currIconUrl);
+                $currIconEl.attr('src', results.currIconUrl);
+                $currDateEl.text('(' + results.currDate + ')');
 
-                    if ('state' in response[0]) {
-                        let cityState = response[0].state;
-                        $currNameEl.text(cityName + ', ' + cityState + ', ' + cityCountry + ' (' + results.currDate + ')')
-                            .append($currIconEl);
-                    } else {
-                        $currNameEl.text(cityName + ', ' + cityCountry + ' (' + results.currDate + ')')
-                            .append($currIconEl);
-                    };
+                if ('state' in response[0]) {
+                    let cityState = response[0].state;
+                    $currNameEl.text(cityName + ', ' + cityState + ', ' + cityCountry);
+                } else {
+                    $currNameEl.text(cityName + ', ' + cityCountry);
+                };
 
-                    // Populating current weather details for selected location
-                    $currTempEl.text(results.currTemp + '℉');
-                    $currWindEl.text(results.currWind + ' MPH');
-                    $currHumidEl.text(results.currHumid + '%');
-                    $currUviEl.text(results.currUvi);
-                    if (results.currUvi <= 2) {
-                        $currUviEl.addClass('bg-success');
-                    } else if (results.currUvi > 2 && results.currUvi <= 7) {
-                        $currUviEl.addClass('bg-warning');
-                    } else {
-                        $currUviEl.addClass('bg-danger');
-                    };
+                // Populating current weather details for selected location
+                $currTempEl.text(results.currTemp + '℉');
+                $currWindEl.text(results.currWind + ' MPH');
+                $currHumidEl.text(results.currHumid + '%');
+                $currUviEl.text(results.currUvi);
+                if (results.currUvi <= 2) {
+                    $currUviEl.addClass('bg-success');
+                } else if (results.currUvi > 2 && results.currUvi <= 7) {
+                    $currUviEl.addClass('bg-warning');
+                } else {
+                    $currUviEl.addClass('bg-danger');
+                };
 
-                    // Populating forecast weather details for selected location
-                    for (let i = 0; i < 5; i++) {
-                        let forecastDay = i + 1;
-                        let indexDate = 'fCastDate_' + forecastDay;
-                        let indexTemp = 'fCastTemp_' + forecastDay;
-                        let indexWind = 'fCastWind_' + forecastDay;
-                        let indexHumid = 'fCastHumid_' + forecastDay;
-                        let indexIcon = 'fCastIcon_' + forecastDay;
+                // Populating forecast weather details for selected location
+                for (let i = 0; i < 5; i++) {
+                    let forecastDay = i + 1;
+                    let indexDate = 'fCastDate_' + forecastDay;
+                    let indexTemp = 'fCastTemp_' + forecastDay;
+                    let indexWind = 'fCastWind_' + forecastDay;
+                    let indexHumid = 'fCastHumid_' + forecastDay;
+                    let indexIcon = 'fCastIcon_' + forecastDay;
 
-                        $fcDateEl[i].textContent = results[indexDate];
-                        $fcTempEl[i].textContent = results[indexTemp] + '℉';
-                        $fcWindEl[i].textContent = results[indexWind] + ' MPH';
-                        $fcHumidEl[i].textContent = results[indexHumid] + '%';
-                        $fcIconEl[i].src = results[indexIcon];
-                    };
+                    $fcDateEl[i].textContent = results[indexDate];
+                    $fcTempEl[i].textContent = results[indexTemp] + '℉';
+                    $fcWindEl[i].textContent = results[indexWind] + ' MPH';
+                    $fcHumidEl[i].textContent = results[indexHumid] + '%';
+                    $fcIconEl[i].src = results[indexIcon];
+                };
 
-                });
-            } else {
-                $errBanner.show();
-            };
+                $plusEl.prop('disabled', false);
+
+            });
         }).catch(function(error) {
+            console.log(error);
             $searchInput.css({
                 'outline-style': 'solid',
                 'outline-color': 'red'
             });
             $errBanner.show();
+        });
+    });
+
+    $plusEl.on('click', function() {
+        let $favBtn = $('<button>');
+        let $cityDesc = $('<div>');
+        let $cityTemp = $('<div>');
+        let $cityIcon = $('<img>');
+
+        $cityDesc.text($currNameEl.text())
+            .addClass('flex-grow-1');
+        $cityTemp.text($currTempEl.text());
+        $cityIcon.attr('src', $currIconEl.attr('src'));
+
+        $favBtn.attr('type', 'button')
+            .addClass('d-flex align-items-center list-group-item list-group-item-action custom-fav')
+            .append($cityDesc, $cityTemp, $cityIcon);
+
+        $favoritesEL.prepend($favBtn);
+
+
+    });
+
+    $(document).on('click', '.custom-fav', function() {
+        let selected = this
+        $minusEl.prop('disabled', false);
+
+        $minusEl.on('click', function() {
+            selected.remove();
         });
     });
 });
